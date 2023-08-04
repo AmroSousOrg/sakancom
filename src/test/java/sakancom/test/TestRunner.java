d5ed26c650f86374f628c1d88806b092f9f3530f
AmroSous <amroosous@gmail.com>
2023-08-04 14:00:57 +0300

    disable test runner

--- src/test/java/sakancom/test/TestRunner.java
+++ src/test/java/sakancom/test/TestRunner.java
@@ -2,16 +2,17 @@ package sakancom.test;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
+//@RunWith(Cucumber.class)
+/*@CucumberOptions(
         features = "src/test/resources/features",
         plugin = {"html:target/HtmlReports/testReport.html"},
         glue = {"sakancom.test"},
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         monochrome = true
-)
+)*/
 
 public class TestRunner {
 }
