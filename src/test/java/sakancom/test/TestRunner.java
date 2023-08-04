fd68b355584678907a87f0c117327769ef1e55c1
AmroSous <amroosous@gmail.com>
2023-08-04 14:12:44 +0300

    enable test runner

--- src/test/java/sakancom/test/TestRunner.java
+++ src/test/java/sakancom/test/TestRunner.java
@@ -2,17 +2,16 @@ package sakancom.test;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-/*@CucumberOptions(
-        features = "src/test/resources/features",
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
         plugin = {"html:target/HtmlReports/testReport.html"},
         glue = {"sakancom.test"},
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         monochrome = true
-)*/
+)
 
 public class TestRunner {
 }
